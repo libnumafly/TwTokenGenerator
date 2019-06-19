9a057fc73d32662e80877bb5c7518ff348a988bc
numaf1y <t.numaf1y+github@gmail.com>
2019-06-19 10:32:02 +0900

    update

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-1234567890
+<html>
+
+    <header>
+        <link rel="stylesheet" href="./material.min.css">
+        <script src="./material.min.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    </header>
+
+    <body>
+
+        Consumer API key
+        <form action="#">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="key">
+                <label class="mdl-textfield__label" for="sample3">Text...</label>
+            </div>
+        </form>
+
+        Consumer API secret key
+        <form action="#">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="secret">
+                <label class="mdl-textfield__label" for="sample3">Text...</label>
+            </div>
+        </form>
+
+    </body>
+
+</html>
