ffac4473aa90580c1ab10ab5e121b8730c075a61
numaf1y <t.numaf1y+github@gmail.com>
2019-06-19 10:39:07 +0900

    update

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
 
     <header>
-        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+        <link rel="stylesheet" href="./material.min.css">
+        <link rel="stylesheet" href="./material.indigo-pink.min.css">
         <script src="./material.min.js"></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="./icon.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </header>
 
