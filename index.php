49d2e474b489ca0d179fe14a43627d497715ec00
numaf1y <t.numaf1y+github@gmail.com>
2019-06-19 10:57:33 +0900

    update

--- index.php
+++ index.php
@@ -13,12 +13,12 @@
         <form action="./getToken.php" method="post">
             Consumer API key
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" id="a1">
+                <input class="mdl-textfield__input" type="text" id="a1" name="a1">
                 <label class="mdl-textfield__label" for="sample3">Key...</label>
             </div>
             Consumer API secret key
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" id="a2">
+                <input class="mdl-textfield__input" type="text" id="a2" name="a2">
                 <label class="mdl-textfield__label" for="sample3">Secret...</label>
             </div>
             <!-- Raised button -->
