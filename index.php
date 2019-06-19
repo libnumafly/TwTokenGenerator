35d2e5adc8ba0995dd4a636902ef241e1881d5eb
numaf1y <t.numaf1y+github@gmail.com>
2019-06-20 00:37:55 +0900

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </header>
 
-<body style="width: calc(100% - 4px); margin-top:2px; margin-bottom: 2px; margin-right: auto; margin-left : auto;">
+<body style="margin-top:2px; margin-bottom: 2px; margin-right: 2px; margin-left : 2px;">
 
     <form action="./tokenGet.php" method="post">
         Consumer API key
