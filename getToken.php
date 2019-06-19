d0947877467d9c1918f462e5b1683b81180c388c
numaf1y <t.numaf1y+github@gmail.com>
2019-06-19 13:45:59 +0900

    update

--- getToken.php
+++ getToken.php
@@ -34,6 +34,7 @@ try {
 		}
 		$_SESSION['oauth_token'] = $res['oauth_token'];
 		$_SESSION['oauth_token_secret'] = $res['oauth_token_secret'];
+		session_write_close();
 
 		// リクエストトークンを持ってTiwtterの認証画面に行くリンクを表示
 		header('Content-Type: text/html');
